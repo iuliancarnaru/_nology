c14ad281f16214538b41cf020c6b86517a452135
Iulian Carnaru <iulian.carnaru@gmail.com>
2020-07-31 16:12:47 +0100

    Updates

---
+++ precache-manifest.0bb2ccd39300546a417e8c9a64f3469d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997a0184bffee1f7eb62f7545f565b30",
+    "url": "/_nology/index.html"
+  },
+  {
+    "revision": "2a71aa473e492fe64232",
+    "url": "/_nology/static/js/2.714ce608.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/_nology/static/js/2.714ce608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02f9429e3514a19a10fd",
+    "url": "/_nology/static/js/main.aab0f6f3.chunk.js"
+  },
+  {
+    "revision": "a491cad6edbafcfa8004",
+    "url": "/_nology/static/js/runtime-main.13ebdb2d.js"
+  }
+]);
